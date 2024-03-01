64fa57376e440b969aaa203ea41188adab05945f
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-02-29 22:04:30 -0300

    feat: criado domanio resposanval pela tipo de folha com funções basicas de crud

--- src/domain/tipoFolha/tipoFolhaApi.ts
+++ src/domain/tipoFolha/tipoFolhaApi.ts
@@ -1,13 +1,10 @@
 import { axiosInstance } from '../../service/config';
 import { TipoFolha } from './types';
 
-export const getTipoFolha = async (): Promise<TipoFolha[]> =>{
+const get = async (): Promise<TipoFolha[]> =>{
 	return new Promise((resolve,reject) => {
 		axiosInstance.get('api/v1/tipo_folha')
 			.then((response) => {
-				console.log('====================================');
-				console.log(response.data);
-				console.log('====================================');
 				resolve(response.data);
 			})
 			.catch((error) => { 
@@ -15,7 +12,67 @@ export const getTipoFolha = async (): Promise<TipoFolha[]> =>{
 			});
 	});
 };  
+const getById = async (id: number): Promise<TipoFolha> =>{
+	return new Promise((resolve,reject) => {
+		axiosInstance.get('api/v1/tipo_folha/' + id)
+			.then((response) => {
+				resolve(response.data);
+			})
+			.catch((error) => { 
+				reject(error);
+			});
+	});
+};  
+const post = async ({
+	tipo_folha
+}: TipoFolha): Promise<TipoFolha> => {
+	return new Promise((resolve, reject) => {
+		axiosInstance.post('api/v1/tipo_folha',
+			{
+				tipo_folha,
+			}
+		)
+			.then((response) => {
+				resolve(response.data);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+};
 
+const put = async ({
+	tipo_folha,
+	id
+}: TipoFolha): Promise<TipoFolha> => {
+	return new Promise((resolve, reject) => {
+		axiosInstance.put('api/v1/tipo_folha/' + id,
+			{
+				tipo_folha,
+			}
+		)
+			.then((response) => {
+				resolve(response.data);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+};
+
+const del = async ({
+	id
+}: TipoFolha): Promise<void> => {
+	return new Promise((resolve, reject) => {
+		axiosInstance.delete('api/v1/tipo_folha/' + id)
+			.then((response) => {
+				resolve(response.data);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+};
 export const TipoFolhaApi = {
-	getTipoFolha
+	get,post,put,del,getById
 };
--- src/domain/tipoFolha/tipoFolhaService.ts
+++ src/domain/tipoFolha/tipoFolhaService.ts
@@ -1,13 +1,68 @@
 import { TipoFolhaApi } from './tipoFolhaApi';
 import { TipoFolha } from './types';
 
-export const getTipoFolha = async (): Promise<TipoFolha[]> => {
-	return new Promise((resolve) => {
-		const tipoFolha = TipoFolhaApi.getTipoFolha();
-		resolve(tipoFolha);
+const get = async (): Promise<TipoFolha[]> => {
+	return new Promise((resolve, reject) => {
+		TipoFolhaApi.get()
+			.then((response) => {
+				resolve(response);
+
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+};
+const getById = async (id: number): Promise<TipoFolha> => {
+	return new Promise((resolve, reject) => {
+		TipoFolhaApi.getById(id)
+			.then((response) => {
+				resolve(response);
+
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+};
+const post = async (tipofolha: TipoFolha): Promise<TipoFolha> => {
+	return new Promise((resolve, reject) => {
+		TipoFolhaApi.post(tipofolha)
+			.then((response) => {
+				resolve(response);
+
+			})
+			.catch((error) => {
+				reject(error);
+			});
 	});
 };
 
+const put = async (tipofolha: TipoFolha): Promise<TipoFolha> => {
+	return new Promise((resolve, reject) => {
+		TipoFolhaApi.put(tipofolha)
+			.then((response) => {
+				resolve(response);
+
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+};
+
+const del = async (tipofolha: TipoFolha): Promise<void> => {
+	return new Promise((resolve, reject) => {
+		TipoFolhaApi.del(tipofolha)
+			.then((response) => {
+				resolve(response);
+
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+};
 export const TipoFolhaService = {
-	getTipoFolha
+	get, put, post, del, getById
 };
