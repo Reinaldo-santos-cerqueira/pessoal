e67706df606ce620415dc78cb9f70de2eed0494c
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-02-29 21:54:16 -0300

    feat: criado elemento topbar resposavel pelo header do sistema

--- src/components/topBar/topBar.tsx
+++ src/components/topBar/topBar.tsx
@@ -1,7 +1,9 @@
-import { AppBar, Box, Drawer, IconButton,Toolbar, Typography } from '@mui/material';
 import React, { useState } from 'react';
-import {Menu as Hambuguer} from '@mui/icons-material';
 import { Menu } from '../menu/menu';
+import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
+import {
+	Menu as HamburgerMenu
+} from '@mui/icons-material';
 
 export function TopBar(): React.ReactNode {
 	const [open, setOpen] = useState(false);
@@ -10,32 +12,16 @@ export function TopBar(): React.ReactNode {
 		setOpen(!open);
 	};
 	return (
-		<Box>
-			<Box sx={{ flexGrow: 1 }}>
-				<AppBar position='fixed' style={{ backgroundColor: '#3e578d'}}>
-					<Toolbar className='relative flex'>
-						<IconButton
-							size="large"
-							edge="start"
-							color="inherit"
-							aria-label="menu"
-							sx={{ mr: 2 }}
-							onClick={changeOpen}
-						>
-							<Hambuguer />
-						</IconButton>
-						<div className='flex  items-center justify-center flex-1 '>
-							<Typography variant='h5' className='font-bold uppercase'>Modulo pessoal</Typography>
-						</div>
-					</Toolbar>
-				</AppBar>
-			</Box>
-			<Drawer
-				anchor={'left'}
-				open={open}
-			>
-				<Menu changeOpen={changeOpen}/>
-			</Drawer>
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position='relative' color='primary' style={{ paddingLeft: open ? 250 : 0, transition:'all 1s'}}>
+				<Toolbar>
+					<IconButton onClick={changeOpen}>
+						<HamburgerMenu style={{color: 'white'}} />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+			<Menu open={open} />
+
 		</Box>
 	);
 }	
